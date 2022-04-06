66046ef4c01c6cd5cbbfb43aa10bc69def5ccd58
rukimhy <rukimhy@gmail.com>
2022-04-06 11:07:21 +0900

    update react

--- js/react/blog/src/App.js
+++ js/react/blog/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
 
@@ -9,16 +10,59 @@ function App() {
     return 100;
   }
 
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '우동 맛집 추천', '감자 음식점']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 제목바꾸기() {
+    let newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    newArray.sort();
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
+
       <div className="black-nav">
         <div className={posts}>개발 Blog</div>
       </div>
-      <img src={logo} />
+
       <h4 style={{ color: 'blue', fontSize: '30px' }}>{posts}</h4>
       <h4>{fn()}</h4>
+
+      <button onClick={제목바꾸기}>남자💨여자💨정렬</button>
+
+      <div className="list">
+        <h3> {글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[1]}</h3>
+        <p>2월 18일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[2]}</h3>
+        <p>2월 19일 발행</p>
+        <hr />
+      </div>
+
+      <Modal />
+
+      <img src={logo} />
     </div >
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
