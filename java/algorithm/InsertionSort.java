f131b27971c7ee96d21179e88a1e4b6214df53c2
rukimhy <rukimhy@gmail.com>
2022-04-19 22:39:53 +0900

    to InsertionSort

---
+++ java/algorithm/InsertionSort.java
@@ -0,0 +1,36 @@
+public class InsertionSort {
+    public static void main(String args[]) {
+        /**
+         * insertion sort = after comparing elements to the left shift elements to the
+         * right to make room to insert a value
+         * 
+         * Quadratic time O(n^2)
+         * small data set = decent
+         * large data set = BAD
+         * 
+         * Less steps than Bubble Sort
+         * Best case is O(n) compared to Selection Sort O(n^2)
+         */
+
+        int array[] = { 9, 1, 8, 2, 7, 3, 6, 5, 4 };
+
+        insertionSort(array);
+
+        for (int i : array) {
+            System.out.print(i);
+        }
+    }
+
+    public static void insertionSort(int array[]) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+    }
+}
