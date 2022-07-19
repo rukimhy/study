23f026b79553044c17c88bfcfb66bc3584d12b25
rukimhy <rukimhy@gmail.com>
2022-07-19 23:42:54 +0900

    ts test

--- ts/test.ts
+++ ts/test.ts
@@ -1,4 +1,8 @@
-class Test {
+interface InterfaceTest {
+	show(): void;
+}
+
+class Test implements InterfaceTest {
 	num: number;
 	str: string;
 
@@ -8,9 +12,35 @@ class Test {
 	}
 
 	show() {
-		console.log('num=' + this.num + ' str=' + this.str);
+		console.log('num:' + this.num + ' str:' + this.str);
 	}
 }
 
-let test: Test = new Test(1, 'test');
-test.show();
+class Test2 implements InterfaceTest {
+	num: number;
+	str: string;
+
+	constructor(num?: number, str?: string) {
+		this.num = 2;
+		if (num) {
+			this.num = num;
+		}
+
+		this.str = 'hello';
+		if (str) {
+			this.str = str;
+		}
+	}
+
+	show() {
+		console.log('num2:' + this.num + ' str2:' + this.str);
+	}
+}
+
+function makeShow(test: InterfaceTest) {
+	test.show();
+}
+
+makeShow(new Test(1, 'test'));
+makeShow(new Test2());
+makeShow(new Test2(3, 'world'));
