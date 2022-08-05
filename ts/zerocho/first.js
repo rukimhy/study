21a82bdb5f3ba8568b41b64204987ba87d793577
rukimhy <rukimhy@gmail.com>
2022-08-05 16:30:07 +0900

    remove js file in ts

--- ts/zerocho/first.js
+++
@@ -1,9 +0,0 @@
-var add = function (x, y) { return x + y; };
-console.log('add(2, 3):' + add(3, 3));
-var arrObj = { lat: 37.5, lon: 127.5 };
-var arrGeneric = [123, 456];
-var arrTupple = [123, 456, 'hello'];
-console.log('arrObj.lat:' + arrObj.lat + '\narrGeneric:' + arrGeneric + '\narrTupple:' + arrTupple);
-var greet = 'hello hell';
-var keyObj = { a: '123', b: 'hello', c: 'world' };
-var zerocho = { breath: true, breed: true, think: true };
